fef12d7ef5898ac6b14896a29684f6f852caae80
James McNeil <mcneil_james@hotmail.com>
2022-04-02 18:03:33 +0100

    created initial form

--- client/src/components/NewExpenseForm.jsx
+++ client/src/components/NewExpenseForm.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
 
 const NewExpenseForm = () => {
-  return <div>NewExpenseForm</div>
+  return (
+    <div className='form max-w-sm mx-auto w-96'>
+      <h1 className='font-bold pb-4 text-xl'>Transaction</h1>
+      <form action='' id='form'>
+        <div className='grid gap-4'>
+          <div className='input-group'>
+            <input
+              type='text'
+              placeholder='Salary, Rent, Food, Drink, Utilities'
+              className='form-input'
+            />
+          </div>
+          <select className='form-input'>
+            <option value='Investment' defaultValue>
+              Investment
+            </option>
+            <option value='Expense'>Expense</option>
+            <option value='Saving'>Saving</option>
+          </select>
+          <div className='input-group'>
+            <input type='text' placeholder='Amount' className='form-input' />
+          </div>
+          <div className='submit-btn'>
+            <button className='border py-2 text-white bg-indigo-500 w-full'>
+              Make Transaction
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default NewExpenseForm
