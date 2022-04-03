2451ca46b3304f71033694d8cdf6c50f1053e01a
James McNeil <mcneil_james@hotmail.com>
2022-04-03 13:34:03 +0100

    used simple state to put put form inputs to console

--- client/src/components/NewExpenseForm.jsx
+++ client/src/components/NewExpenseForm.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NewExpenseForm = () => {
+  const [title, setTitle] = useState('')
+  const [label, setLabel] = useState('')
+  const [amount, setAmount] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(title, label, amount)
+    resetForm()
+  }
+  const resetForm = () => {
+    setAmount('')
+    setLabel('Investment')
+    setTitle('')
+  }
+
   return (
     <div className='form max-w-sm mx-auto w-96'>
       <h1 className='font-bold pb-4 text-xl'>Transaction</h1>
-      <form action='' id='form'>
+      <form onSubmit={handleSubmit} id='form'>
         <div className='grid gap-4'>
           <div className='input-group'>
             <input
               type='text'
               placeholder='Salary, Rent, Food, Drink, Utilities'
               className='form-input'
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
             />
           </div>
-          <select className='form-input'>
+          <select
+            className='form-input'
+            onChange={(e) => setLabel(e.target.value)}
+            value={label}>
             <option value='Investment' defaultValue>
               Investment
             </option>
@@ -21,7 +41,13 @@ const NewExpenseForm = () => {
             <option value='Saving'>Saving</option>
           </select>
           <div className='input-group'>
-            <input type='text' placeholder='Amount' className='form-input' />
+            <input
+              type='text'
+              placeholder='Amount'
+              className='form-input'
+              onChange={(e) => setAmount(e.target.value)}
+              value={amount}
+            />
           </div>
           <div className='submit-btn'>
             <button className='border py-2 text-white bg-indigo-500 w-full'>
