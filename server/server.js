55d37fcf29a172df5509b177f3baa1f5a6edccea
James McNeil <mcneil_james@hotmail.com>
2022-04-03 21:00:02 +0100

    created Controller

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const PORT = 5000
 app.use(cors())
 app.use(express.json())
 
+// using routes
+app.use(require('./routes/routes'))
+
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`)
 })
