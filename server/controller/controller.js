9a27c94eeea585bf276d6f9ae5c63308d9c1e0b1
James McNeil <mcneil_james@hotmail.com>
2022-04-05 08:19:43 +0100

    created model and model controller

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,6 +1,20 @@
+const model = require('../models/model')
+
 // get categories
 const createCategories = (req, res) => {
-  res.json('Get Request from Categories')
+  const Create = new model.Categories({
+    type: 'Saving',
+    color: '#1F3B5C',
+  })
+
+  Create.save((err) => {
+    if (!err) {
+      return res.json(Create)
+    }
+    return res
+      .status(400)
+      .json({ message: `Error while creating categories ${err}` })
+  })
 }
 
 module.exports = {
