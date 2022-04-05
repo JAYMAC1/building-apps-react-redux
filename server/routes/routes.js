c517c8ec9f4c3bbea590f39fcdfaaaf0eaee19f2
James McNeil <mcneil_james@hotmail.com>
2022-04-05 08:19:20 +0100

    set post request route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,6 @@
 const routes = require('express').Router()
 const controller = require('../controller/controller')
 
-routes.route('/api/categories').get(controller.createCategories)
+routes.route('/api/categories').post(controller.createCategories)
 
 module.exports = routes
