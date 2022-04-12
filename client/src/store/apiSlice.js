1e6da091efe62d718d46021fecd9fe563296c2c0
James McNeil <mcneil_james@hotmail.com>
2022-04-12 09:03:22 +0100

    added getLabels getTransaction deleteTransactions

--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -3,9 +3,33 @@ const baseURI = 'http://localhost:8080'
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: baseURI }),
   endpoints: (builder) => ({
+    // Get Categories
     getCategories: builder.query({
       // get request 'http://localhost:8080'
       query: () => '/api/categories',
+    }),
+
+    // Get Labels
+    getLabels: builder.query({
+      query: () => '/api/labels',
+    }),
+
+    // add new transaction
+    addTransaction: builder.mutation({
+      query: (newTransaction) => ({
+        url: '/api/transaction',
+        method: 'POST',
+        body: newTransaction,
+      }),
+    }),
+
+    // delete a transaction
+    deleteTransaction: builder.mutation({
+      query: (transactionID) => ({
+        url: '/api/transaction',
+        method: 'DELETE',
+        body: transactionID,
+      }),
     }),
   }),
 })
