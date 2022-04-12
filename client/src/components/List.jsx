780a18c50fce0d87034f5b17239f7d7aa2c7d4c4
James McNeil <mcneil_james@hotmail.com>
2022-04-12 11:42:24 +0100

    deleted static test obj pulled data from db using api

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,48 +1,51 @@
 import React from 'react'
+import { default as api } from '../store/apiSlice'
 import 'boxicons'
 
-const obj = [
-  {
-    id: 1,
-    type: 'Savings',
-    color: 'rgb(255, 99, 132)',
-    percent: 45,
-  },
-  {
-    id: 2,
-    type: 'Investments',
-    color: 'rgb(54, 162, 235',
-    percent: 20,
-  },
-  {
-    id: 3,
-    type: 'Expense',
-    color: 'rgb(255, 205, 86',
-    percent: 10,
-  },
-]
-
 const List = () => {
+  const { data, isFetching, isError, isSuccess } = api.useGetLabelsQuery()
+  const [deleteTransaction] = api.useDeleteTransactionMutation()
+  const handleClick = (e) => {
+    e.preventDefault()
+    if (!e.target.dataset.id) {
+      return 0
+    } else {
+      deleteTransaction({ _id: e.target.dataset.id })
+    }
+  }
+
+  let Transactions
+  if (isFetching) {
+    Transactions = <div>Fetching transaction data.....</div>
+  } else if (isSuccess) {
+    Transactions = data.map((label, i) => (
+      <Transaction handler={handleClick} key={i} category={label} />
+    ))
+  } else if (isError) {
+    Transactions = <div>Error getting transaction data....</div>
+  }
+
   return (
     <div className='flex flex-col py-6 gap-3'>
       <h1 className='py-4 font-bold text-xl'>History</h1>
-      {obj.map((item) => (
-        <Transaction key={item.id} category={item} />
-      ))}
+      {Transactions}
     </div>
   )
 }
 
-const Transaction = ({ category }) => {
+const Transaction = ({ category, handler }) => {
   if (!category) return null
   return (
     <div
       className='item flex justify-center bg-gray-50 py-2 rounded-r'
       style={{ borderRight: `8px solid ${category.color}` }}>
-      <button className='px-3'>
-        <box-icon color={category.color} name='trash'></box-icon>
+      <button className='px-3' onClick={handler}>
+        <box-icon
+          data-id={category.id ?? ''}
+          color={category.color}
+          name='trash'></box-icon>
       </button>
-      <span className='block w-full'>{category.type}</span>
+      <span className='block w-full'>{category.name}</span>
     </div>
   )
 }
